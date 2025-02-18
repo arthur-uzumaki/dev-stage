999665f8fe3d297fc247805c2290ca77e10bf368
arthur-uzumaki <arthursousarabelo@gmail.com>
2025-02-18 14:42:04 -0300

    feat: adding fonts and color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main className="h-screen flex items-center justify-center">
+      <h1 className="font-heading">Oi</h1>
       <p className="text-red-500 font-bold text-4xl">Hello world!</p>
     </main>
-  );
+  )
 }
